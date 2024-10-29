90ee11e9f6fc2bf8a143b2f126845d726ef5559e
pmandler-umass <pmandler@umass.edu>
2024-10-29 13:42:07 -0400

    Adjust timeline to work in both mobile and web

--- src/components/Timeline.tsx
+++ src/components/Timeline.tsx
@@ -1,60 +1,37 @@
+import { useState } from 'react';
 import { Slider } from '@mantine/core';
+import { isMobile } from '../utils/mobile';
 
-  // Displays tick marks and labels 
-  const marks = [
-    { value: 1, label: 'Jan' },
-    { value: 2 },
-    { value: 3 },
-    { value: 4 },
-    { value: 5 },
-    { value: 6, label: 'Feb' },
-    { value: 7 },
-    { value: 8 },
-    { value: 9 },
-    { value: 10, label: 'Mar' },
-    { value: 11 },
-    { value: 12 },
-    { value: 13 },
-    { value: 14, label: 'Apr' },
-    { value: 15 },
-    { value: 16 },
-    { value: 17 },
-    { value: 18, label: 'May' },
-    { value: 19 },
-    { value: 20 },
-    { value: 21 },
-    { value: 22 },
-    { value: 23, label: 'Jun' },
-    { value: 24 },
-    { value: 25 },
-    { value: 26 },
-    { value: 27, label: 'Jul' },
-    { value: 28 },
-    { value: 29 },
-    { value: 30 },
-    { value: 31 },
-    { value: 32, label: 'Aug' },
-    { value: 33 },
-    { value: 34 },
-    { value: 35 },
-    { value: 36, label: 'Sep' },
-    { value: 37 },
-    { value: 38 },
-    { value: 39 },
-    { value: 40, label: 'Oct' },
-    { value: 41 },
-    { value: 42 },
-    { value: 43 },
-    { value: 44 },
-    { value: 45, label: 'Nov' },
-    { value: 46 },
-    { value: 47 },
-    { value: 48 },
-    { value: 49, label: 'Dec' },
-    { value: 50 },
-    { value: 51 },
-    { value: 52 },
-  ];
+// Displays tick marks and labels 
+const marks = [
+  { value: 1, label: 'Jan' },
+  { value: 6, label: 'Feb' },
+  { value: 10, label: 'Mar' },
+  { value: 14, label: 'Apr' },
+  { value: 18, label: 'May' },
+  { value: 23, label: 'Jun' },
+  { value: 27, label: 'Jul' },
+  { value: 32, label: 'Aug' },
+  { value: 36, label: 'Sep' },
+  { value: 40, label: 'Oct' },
+  { value: 45, label: 'Nov' },
+  { value: 49, label: 'Dec' },
+];
+
+const dateLabels = ['Jan 1', 'Jan 8', 'Jan 15', 'Jan 22', 'Jan 29',
+'Feb 5', 'Feb 12', 'Feb 19', 'Feb 26', 
+'Mar 3', 'Mar 10', 'Mar 17', 'Mar 24', 'Mar 31',
+'Apr 7', 'Apr 14', 'Apr 21', 'Apr 28', 
+'May 5', 'May 12', 'May 19', 'May 26',
+'Jun 2', 'Jun 9', 'Jun 16', 'Jun 23', 'Jun 30',
+'Jul 7', 'Jul 14', 'Jul 21', 'Jul 28',
+'Aug 4', 'Aug 11', 'Aug 18', 'Aug 25',
+'Sep 1', 'Sep 8', 'Sep 15', 'Sep 22', 'Sep 29',
+'Oct 6', 'Oct 13', 'Oct 20', 'Oct 27',
+'Nov 3', 'Nov 10', 'Nov 17', 'Nov 24', 
+'Dec 1', 'Dec 8', 'Dec 15', 'Dec 22', 'Dec 29',
+];
+ 
 
 // Keeps track of the props and prop type going into the component (look up interfaces in TypeScript)
 interface TimelineProps {
@@ -64,23 +41,43 @@ interface TimelineProps {
 
 /* Creates a custom timeline slider that updates what week number of the year the user is currently on. */
 function Timeline(props: TimelineProps) {
-
-
   const { week, onChangeWeek } = props;
+  const [weekLabel, setWeekLabel] = useState<string>('');
+
+  function changeWeekLabel(val: number) {
+    setWeekLabel(dateLabels[val]);
+    onChangeWeek(val);
+  }
 
   return (
+
     <div className="Timeline">
+      {isMobile()?
       <Slider
         defaultValue={week}
         value={week}
+        label={weekLabel}
+        min={1}
+        max={52}
+        labelAlwaysOn
+        size='sm'
+        step={1}
+        thumbSize={12}
+        onChange={(v) => changeWeekLabel(v)}
+      />
+      : 
+      <Slider
+        defaultValue={week}
+        value={week}
+        label={weekLabel}
         marks={marks}
         min={1}
         max={52}
         labelAlwaysOn
         step={1}
         thumbSize={20}
-        onChange={(v) => onChangeWeek(v)}
-      />
+        onChange={(v) => changeWeekLabel(v)}
+      />}
     </div>
   );
 }
