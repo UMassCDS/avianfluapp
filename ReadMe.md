e9c149100c985df7f19d97d0b2f14d35371a8b25
pmandler-umass <pmandler@umass.edu>
2024-12-05 15:34:04 -0500

    Add date labels from Ethan.

--- ReadMe.md
+++ ReadMe.md
@@ -1,16 +1,17 @@
 # Avian Flu App
 
+abundance and movement dates are from https://github.com/birdflow-science/BirdFlowWork/tree/main/avian_influenza/dates
+
 ## Installing Node.js
+
 In order to run this application, you need to have to install Node.js. Follow the instructions to install it:
 https://nodejs.org/en/download/package-manager
-
 
 ## To run in dev:
 
 npm install
 
 npm run dev
-
 
 ## To create a build locally:
 
@@ -23,5 +24,3 @@ serve -s
 ## To install new packages:
 
 npm install <package> --save
-
-
--- src/components/Timeline.tsx
+++ src/components/Timeline.tsx
@@ -1,15 +1,18 @@
 import { useEffect, useState } from 'react';
 import { Slider } from '@mantine/core';
 import { isMobile } from '../utils/utils';
+import ab_dates from '../assets/abundance_dates.json';
+import mv_dates from '../assets/movement_dates.json';
+
 
 // Displays tick marks and labels 
 const marks = [
   { value: 1, label: 'Jan' },
-  { value: 6, label: 'Feb' },
-  { value: 10, label: 'Mar' },
-  { value: 14, label: 'Apr' },
+  { value: 5, label: 'Feb' },
+  { value: 9, label: 'Mar' },
+  { value: 13, label: 'Apr' },
   { value: 18, label: 'May' },
-  { value: 23, label: 'Jun' },
+  { value: 22, label: 'Jun' },
   { value: 27, label: 'Jul' },
   { value: 32, label: 'Aug' },
   { value: 36, label: 'Sep' },
@@ -18,36 +21,41 @@ const marks = [
   { value: 49, label: 'Dec' },
 ];
 
-const dateLabels = ['Jan 1', 'Jan 8', 'Jan 15', 'Jan 22', 'Jan 29',
-'Feb 5', 'Feb 12', 'Feb 19', 'Feb 26', 
-'Mar 3', 'Mar 10', 'Mar 17', 'Mar 24', 'Mar 31',
-'Apr 7', 'Apr 14', 'Apr 21', 'Apr 28', 
-'May 5', 'May 12', 'May 19', 'May 26',
-'Jun 2', 'Jun 9', 'Jun 16', 'Jun 23', 'Jun 30',
-'Jul 7', 'Jul 14', 'Jul 21', 'Jul 28',
-'Aug 4', 'Aug 11', 'Aug 18', 'Aug 25',
-'Sep 1', 'Sep 8', 'Sep 15', 'Sep 22', 'Sep 29',
-'Oct 6', 'Oct 13', 'Oct 20', 'Oct 27',
-'Nov 3', 'Nov 10', 'Nov 17', 'Nov 24', 
-'Dec 1', 'Dec 8', 'Dec 15', 'Dec 22'
-];
- 
-
 // Keeps track of the props and prop type going into the component (look up interfaces in TypeScript)
 interface TimelineProps {
   week: number;
+  dataset: number;
   onChangeWeek: (val: number) => void;
 }
 
 /* Creates a custom timeline slider that updates what week number of the year the user is currently on. */
 function Timeline(props: TimelineProps) {
   const { week, onChangeWeek } = props;
+  const dataset = props.dataset;
   const [weekLabel, setWeekLabel] = useState<string>('');
+  const [labelInit, setLabelInit] = useState<boolean>(false);
+  const [myLabels, setMyLabels] = useState<Array<Array<string>>>([[],[]]);
+
+  useEffect(() => {
+    // initialize labels[dataset][week]
+    // TODO the order of the labels needs to match the order of datasets in dataUrl.tsx/dataInfo[]
+    let local_dates: Array<Array<string>> = [[],[]];
+    ab_dates.map((info) => (
+      local_dates[0].push(info.label)
+    ))
+    mv_dates.map((info) => (
+      local_dates[1].push(info.label)
+    ))
+    setMyLabels(local_dates);
+    setLabelInit(true);
+  }, []);
 
   useEffect(() => {
     // update the label WHEN the check for overlay is complete
-    setWeekLabel(dateLabels[week-1]);
-  }, [week]);
+    if (labelInit) {
+      setWeekLabel(myLabels[dataset][week-1]);
+    }
+  }, [week, props.dataset]);
 
   return (
     <div className="Timeline">
@@ -59,9 +67,9 @@ function Timeline(props: TimelineProps) {
         min={1}
         max={52}
         labelAlwaysOn
-        size='sm'
+        size='sm' // sm screen
         step={1}
-        thumbSize={12}
+        thumbSize={12}  // sm screen
         onChange={(v) => { onChangeWeek(v)}}
       />
       : 
@@ -69,12 +77,12 @@ function Timeline(props: TimelineProps) {
         defaultValue={week}
         value={week}
         label={weekLabel}
-        marks={marks}
+        marks={marks} // lg screen
         min={1}
         max={52}
         labelAlwaysOn
         step={1}
-        thumbSize={20}
+        thumbSize={20} // lg screen
         onChange={(v) => onChangeWeek(v)}
       />}
     </div>
