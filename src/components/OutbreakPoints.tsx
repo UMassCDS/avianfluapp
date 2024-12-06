1ded884cd7a7fa02fc717b0337c593a9d7e5e6f1
pmandler-umass <pmandler@umass.edu>
2024-12-06 11:09:08 -0500

    use local pngs for icons

--- src/components/OutbreakPoints.tsx
+++ src/components/OutbreakPoints.tsx
@@ -1,23 +1,43 @@
 //import { useEffect, useState } from 'react';
-import {Marker, Popup } from 'react-leaflet';
+import { Marker, Popup } from 'react-leaflet';
+import { Icon } from 'leaflet';
 import geoCounties from '../assets/counties.json';
 import outbreaks from '../assets/outbreaks.json';
+import iconOne from '../assets/iconBlue.png';
+import iconTwo from '../assets/iconLightBlue.png';
+import iconThree from '../assets/iconPaleBlue.png';
 import {monthDayToWeek} from '../utils/utils'
 
 // lat, long position in degrees
 type GeoLocation = [number, number]
 
 type outMarker = {
-    GeoLoc: GeoLocation;
+    geoLoc: GeoLocation;
     year: number;
+    yearsAgo: number;
     week: number;
     label: string;
+}
+
+function outbreakIcon(icon_path: string) {
+    return new Icon({
+        iconUrl: icon_path,
+        iconSize : [35,35], // size of the icon
+        iconAnchor : [18, 35], // the tip of the icon points to the middle bottom of the png
+        popupAnchor : [8, -30] // point from which the popup should open relative to the iconAnchor
+    })
 }
 
 const outbreakMarkers: outMarker[]=[];
 const NUM_YEARS = 3; // number of years back
 const NUM_WEEKS = 2; // number of weeks +/-
 const thisYear = new Date().getFullYear();
+const markerIcons: Icon[] = [
+    outbreakIcon(iconOne),
+    outbreakIcon(iconTwo),
+    outbreakIcon(iconThree),
+    outbreakIcon(iconThree),
+]
 
 export function loadOutbreaks() {
     if (outbreakMarkers.length > 0) {
@@ -51,10 +71,12 @@ export function loadOutbreaks() {
         if (locationDict[outbreak.State][outbreak['County Name'].toUpperCase()] === undefined) {
             console.log(outbreak.State+", "+outbreak['County Name'].toUpperCase());
         } else {
+            const year = Number(outbreak.Confirmed.split('-')[0]);
             let marker:outMarker = {
-                year: Number(outbreak.Confirmed.split('-')[0]),
+                year: year,
+                yearsAgo: thisYear - year,
                 week: monthDayToWeek(Number(outbreak.Confirmed.split('-')[1])-1, Number(outbreak.Confirmed.split('-')[2])),
-                GeoLoc: locationDict[outbreak.State][outbreak['County Name'].toUpperCase()],
+                geoLoc: locationDict[outbreak.State][outbreak['County Name'].toUpperCase()],
                 label: outbreak["County Name"]+','+outbreak.State+' @'+outbreak.Confirmed,
                 // label: outbreak["County Name"]+','+outbreak.State+': '+outbreak.NumInfected+','+outbreak.Production
             }
@@ -70,7 +92,7 @@ function selectedOutbreaks(this_week: number):outMarker[] {
     const markers: outMarker[]=[];
 
     for (var info of outbreakMarkers) {
-        if ((info.year >= thisYear - NUM_YEARS ) && (Math.abs(info.week-this_week) <= NUM_WEEKS)) {
+        if ((info.yearsAgo <= NUM_YEARS ) && (Math.abs(info.week-this_week) < NUM_WEEKS)) {
             markers.push(info);
         }
     }
@@ -83,8 +105,9 @@ export function OutbreakMarkers(week: number) {
     return (
         currentMarkers.map((info, i) => (
             <Marker
-                position={info.GeoLoc}
+                position={info.geoLoc}
                 key={i}
+                icon={markerIcons[info.yearsAgo]}
             >
                 <Popup>
                     {info.label}
