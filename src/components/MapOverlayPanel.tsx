41f74626abbf2f3e17d6f53d0895ba00986f3aaf
Sergei Pogorelov <serge.pogorelov@gmail.com>
2025-07-09 10:20:56 -0400

    feat: add `Data type` and `Species` info to the `MapOverlayPanel`

--- src/components/MapOverlayPanel.tsx
+++ src/components/MapOverlayPanel.tsx
@@ -1,6 +1,7 @@
 import { useSelector } from "react-redux";
 import { RootState } from "../store/store";
 import { dataInfo } from "../hooks/dataUrl";
+import taxa from '../assets/taxa.json';
 import AppHeader from "./AppHeader";
 
 interface MapOverlayPanelProps {
@@ -9,12 +10,15 @@ interface MapOverlayPanelProps {
 
 export default function MapOverlayPanel({ children }: MapOverlayPanelProps) {
   const dataIndex = useSelector((state: RootState) => state.species.dataIndex);
+  const speciesIndex = useSelector((state: RootState) => state.species.speciesIndex);
 
   return (
     <div className="bg-white/30 rounded-xl shadow-lg p-4 w-full">
       <AppHeader />
-      <div className="text-base font-semibold text-blue-800 mb-2 text-center">
-        {dataInfo[dataIndex]?.label}
+      <div className="text-sm text-blue-800 mb-2 text-center">
+        <strong>Data type:</strong> {dataInfo[dataIndex]?.label}
+        <br/>
+        <strong>Species:</strong> {taxa[speciesIndex].label}
       </div>
       {children}
     </div>
